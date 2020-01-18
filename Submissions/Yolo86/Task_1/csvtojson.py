3cf940aef8f55d7fd515a4fc5b91e75df9431340
yolo86 <sakshamsood00@gmail.com>
2020-01-18 16:06:54 +0530

    Fixed json file and modified python script(relative path)

--- Submissions/Yolo86/Task_1/csvtojson.py
+++ Submissions/Yolo86/Task_1/csvtojson.py
@@ -1,20 +1,25 @@
 import csv
 import json
+import os
 
-with open('C:\\Users\\SAKSHAM\\Desktop\\OpenCode20\\2-Python t1\\Monty_Python_Class\\Task_1\\movies.csv','r', encoding="utf8") as csv_file:
+path1 = os.getcwd()
+for i in range(3):
+	path1 = os.path.split(path1)[0]
+path1 = os.path.join(path1,"","Task_1\movies.csv")
+
+
+dict = []
+
+with open(path1,'r', encoding="utf8") as csv_file:
 	fieldnames = ("movieId","title","genres")
 	csv_reader = csv.DictReader(csv_file,fieldnames)
 
-	
-		
-		
-
-	with open('csv2json.json','w', encoding="utf8") as json_file:
-
-		for line in csv_reader:
-			b = line['genres'].split('|')
-
-			for c in b:
-				if c == 'Comedy':
-					json.dump(line,json_file,indent = 4)
-					json_file.write('\n')
+	for line in csv_reader:
+		b = line['genres'].split('|')
+		for c in b:
+			if c == 'Comedy':
+				dict.append(line)
+				break
+					
+with open('csv2json.json','w',encoding = "utf8") as json_file:
+	json.dump(dict,json_file,indent = 4)
