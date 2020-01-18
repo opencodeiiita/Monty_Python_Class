8e0dc1ba646761200d63fef99530405447823504
yolo86 <sakshamsood00@gmail.com>
2020-01-18 13:59:52 +0530

    Added file to conver csv to json

---
+++ Submissions/Yolo86/Task_1/csvtojson.py
@@ -0,0 +1,14 @@
+import csv
+import json
+
+with open('C:\\Users\\SAKSHAM\\Desktop\\OpenCode20\\2-Python t1\\Monty_Python_Class\\Task_1\\movies.csv','r', encoding="utf8") as csv_file:
+	fieldnames = ("movieId","title","genres")
+	csv_reader = csv.DictReader(csv_file,fieldnames)
+
+	with open('csv2json.json','w', encoding="utf8") as json_file:
+
+		for line in csv_reader:
+
+			if line['genres'] == 'Comedy':
+				json.dump(line,json_file,indent = 4)
+				json_file.write('\n')
