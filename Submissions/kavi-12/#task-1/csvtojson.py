9288001db9a02cd5f26ca2a1021ba6af8e910084
kavi-12 <vidurkataria007@gmail.com>
2020-01-19 00:22:24 +0530

    python script to get json file of movies with genre Comedy

--- Submissions/kavi-12/#task-1/csvtojson.py
+++ Submissions/kavi-12/#task-1/csvtojson.py
@@ -6,13 +6,13 @@ data = {}
 with open(csv_path,  encoding='utf-8') as csvfile:
     csvReader = csv.DictReader(csvfile)
     for csvrow in csvReader:
-        
-        obj = {
-            "title": csvrow["title"],
-            "genres": csvrow["genres"]
-            
-        }
-        data[csvrow["movieId"]] = obj
+        if "Comedy" in csvrow["genres"]:
+            obj = {
+                "title": csvrow["title"],
+                "genres": csvrow["genres"]
+                
+            }
+            data[csvrow["movieId"]] = obj
 
 with open(json_path, 'w+') as jsonFile:
     jsonFile.write(json.dumps(data, indent = 4))
