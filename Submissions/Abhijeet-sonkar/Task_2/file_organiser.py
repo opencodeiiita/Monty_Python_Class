749ea281489d88b81ae43a49538b88521c569bab
Abhijeet-sonkar <53171003+Abhijeet-sonkar@users.noreply.github.com>
2020-01-18 17:55:29 +0530

    removed abosolute path and made the necessary changes;

--- Submissions/Abhijeet-sonkar/Task_2/file_organiser.py
+++ Submissions/Abhijeet-sonkar/Task_2/file_organiser.py
@@ -1,30 +1,24 @@
-import os,shutil
+import os,sys,shutil
 def make(dir):
     try:
         if not os.path.exists(dir):
             os.makedirs(dir)
     except OSError:
-            print ('error')    
-        
-files=os.scandir('My_Files/')
+            print ('error') 
+x=sys.argv[1]
+y=os.listdir(x)
 extension=[]
-for i in files:
-     a=str(i.name)
-     i=a.find('.')
-     if a[i+1:] not in extension:
-         extension.append(a[1+i:])
+for i in y:
+     p=i.find('.')
+     if i[p+1:] not in extension:
+         extension.append(i[1+p:]) 
 
 for file in extension:
-    make('./'+file+'/') 
-
-files=os.scandir('My_Files/')
-for i in files:
-   a=str(i.name)
-   y=a.find('.')
+    make('./'+x+'/'+file+'/')              
+for i in y:
+   p=i.find('.')
    for j in extension:
-       if(a[y+1:]==j):
-        source=('/home/arthas/Desktop/python/My_Files/'+i.name)
-        path=('/home/arthas/Desktop/python/' +j)  
-        shutil.copy(source,path)
-
-
+       if(i[p+1:]==j):
+        source=('./'+x+'/'+i)
+        path=('./'+x+'/' +j+'/')  
+        shutil.move(source, path)
