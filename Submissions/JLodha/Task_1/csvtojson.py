abdb1eaa319731df8b151ab2778eba5fb003cdb6
JhX <iit2019134@iiita.ac.in>
2020-01-18 14:22:39 +0530

    Task 1 - CSV To Json

--- Submissions/JLodha/Task_1/csvtojson.py
+++ Submissions/JLodha/Task_1/csvtojson.py
@@ -8,7 +8,7 @@ datalist = []
 movie_genre= 'Comedy'
 for row in reader:
     newrow = {'movieId' : row['movieId'], 'title':row['title'], 'genres': row['genres']}
-    if(newrow['genres'] == movie_genre):
+    if(newrow['genres'].find(movie_genre)!=-1):
         datalist.append(newrow)
 with open('movies.json','w') as jsonfile:
     jsonfile.write(json.dumps(datalist,indent = 4))
