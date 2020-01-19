b096f8d8b7f9e90dbc1c37194578e15a2b3f91a2
yolo86 <sakshamsood00@gmail.com>
2020-01-20 01:47:48 +0530

    Added file to clump all similar files into respective folders

---
+++ Submissions/Yolo86/Task_2/files.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+os.chdir('../../../Task_2/My_Files/')
+b = os.getcwd()
+
+for fi in os.listdir():
+	try:
+		fna,fext = fi.split('.')
+		if os.path.exists(b+'/'+fext):
+			shutil.move(b+'/'+fi,b+'/'+fext+'/'+fi)
+		else:
+			os.makedirs(b+'/'+fext)
+			shutil.move(b+'/'+fi,b+'/'+fext+'/'+fi)
+	except ValueError:
+		continue
+	
+		
