e1134da08f25e7194a46fab8195c08aca6689761
prince-7 <amanvijaykumarsingh7792@gmail.com>
2020-01-21 19:29:11 +0530

    csv2json

---
+++ Submissions/prince-7/task_1/csv2json.py
@@ -0,0 +1,15 @@
+import csv
+import json
+
+csvfile = open('movies.csv','r')
+jsonfile = open('movies.json', 'w')
+fieldnames = ['movieId','title','genres']
+reader = csv.DictReader(csvfile, fieldnames)
+for row in reader:
+    newrow = {'movieId' : row['movieId'], 'title':row['title'], 'genres': row['genres']}
+    if(newrow['genres'].find('Comedy')!=-1):
+        json.dump(newrow,jsonfile)
+        jsonfile.write('\n')
+
+
+
