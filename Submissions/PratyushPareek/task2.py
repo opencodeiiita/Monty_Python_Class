8c4e266cfd30da38447604b431b2d2c226860de2
PratyushPareek <pratyushpareek26@gmail.com>
2020-01-21 20:03:37 +0530

    task #2

--- Submissions/PratyushPareek/task2.py
+++ Submissions/PratyushPareek/task2.py
@@ -2,14 +2,14 @@ import shutil
 import os
 
  
-for folders, subfolders ,files in os.walk('D:\\Shit I made\\Monty_Python_Class\\Task_2\\My_Files\\'):
+for folders, subfolders ,files in os.walk("..\\...\\Monty_Python_Class\\Task_2\\My_Files\\'):
     for file in files:
         name, ext = os.path.splitext(file)
-        if os.path.exists('D:\\Shit I made\\Monty_Python_Class\\Task_2\\My_Files\\'+ (ext.upper())[1:]):
-            shutil.move('D:\\Shit I made\\Monty_Python_Class\\Task_2\\My_Files\\'+str(file), 'D:\\Shit I made\\Monty_Python_Class\\Task_2\\My_Files\\'+(ext.upper())[1:])
+        if os.path.exists('..\\...\\Monty_Python_Class\\Task_2\\My_Files\\'+ (ext.upper())[1:]):
+            shutil.move('..\\...\\Monty_Python_Class\\Task_2\\My_Files\\'+str(file), '..\\...\\Monty_Python_Class\\Task_2\\My_Files\\'+(ext.upper())[1:])
         else:
-            os.mkdir('D:\\Shit I made\\Monty_Python_Class\\Task_2\\My_Files\\'+(ext.upper())[1:])
-            shutil.move('D:\\Shit I made\\Monty_Python_Class\\Task_2\\My_Files\\'+str(file), 'D:\\Shit I made\\Monty_Python_Class\\Task_2\\My_Files\\'+(ext.upper())[1:])
+            os.mkdir('..\\...\\Monty_Python_Class\\Task_2\\My_Files\\'+(ext.upper())[1:])
+            shutil.move('..\\...\\Monty_Python_Class\\Task_2\\My_Files\\'+str(file), '..\\...\\Monty_Python_Class\\Task_2\\My_Files\\'+(ext.upper())[1:])
 
 
 
