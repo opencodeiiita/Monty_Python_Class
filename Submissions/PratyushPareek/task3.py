a587d549bc83d55c7d97b29bcdb4256a9d0910eb
PratyushPareek <58395391+PratyushPareek@users.noreply.github.com>
2020-01-22 21:05:09 +0530

    Task #3 with user input

--- Submissions/PratyushPareek/task3.py
+++ Submissions/PratyushPareek/task3.py
@@ -1,8 +1,14 @@
 import smtplib
 
+senderEmailID = input()
+password = input()
+recieverEmailID = input()
+Subject = input()
+Body = input()
+
 connection = smtplib.SMTP('smtp.gmail.com',587)
 connection.ehlo()
 connection.starttls()
-connection.login('pareekpratyush2626@gmail.com','password')
-connection.sendmail('pareekpratyush2626@gmail.com','iit2019184@iiita.ac.in','Subject: Life is meaningless \nNothing has a purpose\n')
+connection.login(senderEmailID,password)
+connection.sendmail(senderEmailID,recieverEmailID,'Subject: '+Subject+ '\n' + Body)
 
