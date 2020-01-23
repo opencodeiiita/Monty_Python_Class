e57afbcb8b534096e9a53db291aa9dd0a6d0f8f0
aawzard <iec2019097@iiita.ac.in>
2020-01-23 18:06:09 +0530

    Updated code to seprate message and subject of mail

--- Submissions/aawizard/Task_3/email.py
+++ Submissions/aawizard/Task_3/email.py
@@ -2,7 +2,9 @@ import smtplib
 gmailaddress = input("Your gmail address:  ")
 gmailpassword = input("Password for that email address: ")
 reciever = input("Receiver's email address: ")
+subject=input("Enter subject of message")
 msg = input("What is your message?  ")
+FinalMail="Subject:"+subject+"\n"+"Message"+msg
 mailServer = smtplib.SMTP('smtp.gmail.com' , 587)
 mailServer.starttls()
 mailServer.login(gmailaddress , gmailpassword)
