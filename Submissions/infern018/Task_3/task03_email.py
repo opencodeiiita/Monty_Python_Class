886c8b7d0ffcb8f466246523a8e85e07f8ff07d8
infern018 <singh.vishu234@gmail.com>
2020-01-23 12:22:22 +0530

    added python script for sending mail

---
+++ Submissions/infern018/Task_3/task03_email.py
@@ -0,0 +1,10 @@
+
+import smtplib
+
+content= 'HELLO WORLD'
+mail=smtplib.SMTP('smtp.gmail.com',587)
+mail.ehlo()
+mail.starttls()
+mail.login('singhvishu26@gmail.com','A@nmol26')
+mail.sendmail('singhvishu26@gmail.com','iec2019019@iiita.ac.in',content)
+mail.close()
