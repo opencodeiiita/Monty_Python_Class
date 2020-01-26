ecbb0c7701b96802eae2a78b2c1e4bbe7cc55ac7
aawzard <iec2019097@iiita.ac.in>
2020-01-26 16:24:15 +0530

    Now it stores all movies with genres as comedy

--- Submissions/aawizard/Task_1/cvsTojson.py
+++ Submissions/aawizard/Task_1/cvsTojson.py
@@ -3,16 +3,16 @@ import csv, json
 csvFilePath='movies.csv'
 jasonFilePath="movieList.json"
 data={}
-comedy={}
+comedy=[]
+fields = ['movieId','title','genres']
 with open(csvFilePath,encoding = "utf8",errors = 'ignore') as csvFile:
-    csvReader=csv.DictReader(csvFile)
+    csvReader=csv.DictReader(csvFile,fieldnames=fields)
     for csvRow in csvReader:
-        movieId=csvRow["movieId"]
-        data[movieId]=csvRow
-        if csvRow["genres"]=="Comedy":
-            
-            comedy[movieId]=csvRow
-		      
+        newrow = {'movieId' : csvRow['movieId'], 'title':csvRow['title'], 'genres': csvRow['genres']}
+        if(newrow['genres'].find('Comedy')!=-1):
+            comedy.append(newrow)
+        
+#writing to json		      
 with open('movies.json','w') as newfile:
     newfile.write(json.dumps(comedy,indent = 4))        
 
